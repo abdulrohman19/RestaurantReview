1abfe987d1889d4ecae8cb276c3b00f2f6529fa4
abdul rohman <abdulrohmanunida@gmail.com>
2023-10-01 09:26:12 +0700

    add ListAdapter

---
+++ app/src/main/java/com/best/restaurantreview/ui/ReviewAdapter.kt
@@ -0,0 +1,45 @@
+package com.best.restaurantreview.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.best.restaurantreview.data.response.CustomerReviewsItem
+import com.best.restaurantreview.databinding.ItemReviewBinding
+
+class ReviewAdapter : ListAdapter<CustomerReviewsItem, ReviewAdapter.MyViewHolder>(DIFF_CALLBACK) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ItemReviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindingViewHolder(holder: MyViewHolder, position: Int) {
+        val review = getItem(position)
+        holder.bind(review)
+    }
+
+    class MyViewHolder(val binding: ItemReviewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(review: CustomerReviewsItem){
+            binding.tvItem.text = "${review.review}\n- ${review.name}"
+        }
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<CustomerReviewsItem>() {
+            override fun areItemsTheSame(
+                oldItem: CustomerReviewsItem,
+                newItem: CustomerReviewsItem
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: CustomerReviewsItem,
+                newItem: CustomerReviewsItem
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
