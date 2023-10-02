4bb665a256c2b346ae57d86b5a73e27c19bb4efd
abdul rohman <abdulrohmanunida@gmail.com>
2023-10-02 16:44:13 +0700

    Added and Updated

--- app/src/main/java/com/best/restaurantreview/ui/MainActivity.kt
+++ app/src/main/java/com/best/restaurantreview/ui/MainActivity.kt
@@ -3,30 +3,25 @@ package com.best.restaurantreview.ui
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.best.restaurantreview.data.response.CustomerReviewsItem
-import com.best.restaurantreview.data.response.PostReviewResponse
 import com.best.restaurantreview.data.response.Restaurant
-import com.best.restaurantreview.data.response.RestaurantResponse
-import com.best.restaurantreview.data.retrofit.ApiConfig
 import com.best.restaurantreview.databinding.ActivityMainBinding
 import com.bumptech.glide.Glide
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    companion object {
-        private const val TAG = "MainActivity"
-        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
-    }
+    //    companion object {
+//        private const val TAG = "MainActivity"
+//        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
+//    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,78 +30,92 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+        val mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+        mainViewModel.restaurant.observe(this) {restaurant ->
+            setRestaurantData(restaurant)
+        }
+
         val layoutManager = LinearLayoutManager(this)
         binding.rvReview.layoutManager = layoutManager
         val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
         binding.rvReview.addItemDecoration(itemDecoration)
 
-        findRestaurant()
+        mainViewModel.listReview.observe(this) {consumerReviews->
+            setReviewData(consumerReviews)
+        }
 
-        binding.btnSend.setOnClickListener {view ->
-            postReview(binding.edReview.text.toString())
+        mainViewModel.isLoading.observe(this) {
+            showLoading(it)
+        }
+
+//        findRestaurant()
+
+        binding.btnSend.setOnClickListener { view ->
+//            postReview(binding.edReview.text.toString())
+            mainViewModel.postReview(binding.edReview.text.toString())
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
     }
 
-    private fun findRestaurant() {
-        showLoading(true)
-        val client = ApiConfig.getApiService().getRestaurant(RESTAURANT_ID)
-        client.enqueue(object : Callback<RestaurantResponse> {
-            override fun onResponse(
-                call: Call<RestaurantResponse>,
-                response: Response<RestaurantResponse>
-            ) {
-                showLoading(false)
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        setRestaurantData(responseBody.restaurant)
-                        setReviewData(responseBody.restaurant.customerReviews)
-                    }
-                } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
-            }
-
-            override fun onFailure(call: Call<RestaurantResponse>, t:Throwable) {
-                showLoading(false)
-                Log.e(TAG, "onFailure: ${t.message}")
-            }
-        })
-    }
+//    private fun findRestaurant() {
+//        showLoading(true)
+//        val client = ApiConfig.getApiService().getRestaurant(RESTAURANT_ID)
+//        client.enqueue(object : Callback<RestaurantResponse> {
+//            override fun onResponse(
+//                call: Call<RestaurantResponse>,
+//                response: Response<RestaurantResponse>
+//            ) {
+//                showLoading(false)
+//                if (response.isSuccessful) {
+//                    val responseBody = response.body()
+//                    if (responseBody != null) {
+//                        setRestaurantData(responseBody.restaurant)
+//                        setReviewData(responseBody.restaurant.customerReviews)
+//                    }
+//                } else {
+//                    Log.e(TAG, "onFailure: ${response.message()}")
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
+//                showLoading(false)
+//                Log.e(TAG, "onFailure: ${t.message}")
+//            }
+//        })
+//    }
 
     private fun setRestaurantData(restaurant: Restaurant) {
         binding.tvTitle.text = restaurant.name
         binding.tvDescription.text = restaurant.description
-        Glide.with(this@MainActivity)
+        Glide.with(this)
             .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
             .into(binding.ivPicture)
     }
 
-    private fun postReview(review: String) {
-        showLoading(true)
-        val client = ApiConfig.getApiService().postReview(RESTAURANT_ID, "Dicoding", review)
-        client.enqueue(object : Callback<PostReviewResponse> {
-            override fun onResponse(
-                call: Call<PostReviewResponse>,
-                response: Response<PostReviewResponse>
-            ) {
-                showLoading(false)
-                val responseBody = response.body()
-                if (response.isSuccessful && responseBody != null) {
-                    setReviewData(responseBody.customerReviews)
-                } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
-            }
-
-            override fun onFailure(call: Call<PostReviewResponse>, t: Throwable) {
-                showLoading(false)
-                Log.e(TAG, "onFailure: ${t.message}")
-            }
-        })
-    }
+//    private fun postReview(review: String) {
+//        showLoading(true)
+//        val client = ApiConfig.getApiService().postReview(RESTAURANT_ID, "Dicoding", review)
+//        client.enqueue(object : Callback<PostReviewResponse> {
+//            override fun onResponse(
+//                call: Call<PostReviewResponse>,
+//                response: Response<PostReviewResponse>
+//            ) {
+//                showLoading(false)
+//                val responseBody = response.body()
+//                if (response.isSuccessful && responseBody != null) {
+//                    setReviewData(responseBody.customerReviews)
+//                } else {
+//                    Log.e(TAG, "onFailure: ${response.message()}")
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<PostReviewResponse>, t: Throwable) {
+//                showLoading(false)
+//                Log.e(TAG, "onFailure: ${t.message}")
+//            }
+//        })
+//    }
 
     private fun setReviewData(consumerReviews: List<CustomerReviewsItem>) {
         val adapter = ReviewAdapter()
@@ -116,11 +125,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showLoading(isLoading: Boolean) {
-        if (isLoading) {
-            binding.progressBar.visibility = View.VISIBLE
-        } else {
-            binding.progressBar.visibility = View.GONE
-        }
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+//        if (isLoading) {
+//            binding.progressBar.visibility = View.VISIBLE
+//        } else {
+//            binding.progressBar.visibility = View.GONE
+//        }
     }
 
 }
---
+++ app/src/main/java/com/best/restaurantreview/ui/MainViewModel.kt
@@ -0,0 +1,78 @@
+package com.best.restaurantreview.ui
+
+import android.telecom.Call
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.best.restaurantreview.data.response.CustomerReviewsItem
+import com.best.restaurantreview.data.response.PostReviewResponse
+import com.best.restaurantreview.data.response.Restaurant
+import com.best.restaurantreview.data.response.RestaurantResponse
+import com.best.restaurantreview.data.retrofit.ApiConfig
+import retrofit2.Response
+import javax.security.auth.callback.Callback
+
+class MainViewModel : ViewModel() {
+
+    private val_restaurant = MutableLiveData<Restaurant>()
+    val restaurant: LiveData<Restaurant> = _restaurant
+
+    private val _listReview = MutableLiveData<List<CustomerReviewsItem>>()
+    val listReview: LiveData<List<CustomerReviewsItem>> = _listReview
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    companion object{
+        private const val TAG = "MainViewModel"
+        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
+    }
+    init {
+        findRestaurant()
+    }
+
+    private fun findRestaurant() {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getRestaurant(RESTAURANT_ID)
+        client.enqueue(object : Callback<RestaurantResponse> {
+            override fun onResponse(
+                call: Call<RestaurantResponse>,
+                response: Response<RestaurantResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _restaurant.value = response.body()?.restaurant
+                    _listReview.value = response.body()?.restaurant?.customerReviews
+                } else {
+                    Log.e(TAG, "OnFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message.toString()}")
+            }
+        })
+    }
+
+    fun postReview(review: String) {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().postReview(RESTAURANT_ID, "Dicoding", review)
+        client.enqueue(object : Callback<PostReviewResponse> {
+            override fun onResponse(call: Call<PostReviewResponse>, response: Response<PostReviewResponse>) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _listReview.value = response.body()?.customerReviews
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<PostReviewResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message.toString()}")
+            }
+        })
+    }
+}
